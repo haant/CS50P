5dc9a1b9d835577b9d436fd1fd13f68c301e8e3f
Denas Zelvys <95925838+haant@users.noreply.github.com>
2022-04-02 23:47:30 +0100

    Add files via upload

---
+++ Lecture-0-Overview/functions.py
@@ -0,0 +1,37 @@
+def hello():
+    print("hello")
+
+name = input("What is your name?: ")
+
+hello()
+
+print(name)
+
+# OR
+
+def hello(to):
+    print("hello,", to)
+
+name = input("What is your name?: ")
+
+hello(name)
+
+# OR
+
+def hello(to="world"):
+    print("hello,", to)
+
+name = input("What is your name?: ")
+
+hello(name)
+
+# OR
+
+def main():
+    name = input("What is your name?: ")
+    hello(name)
+
+def hello(to="world"):
+    print("hello,", to)
+
+main()
